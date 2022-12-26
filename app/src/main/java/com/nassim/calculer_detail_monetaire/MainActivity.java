045a58251dac1043547f02cc01a046bb2bb93cff
hanzaz-abdelaziz <119833953+hanzaz-abdelaziz@users.noreply.github.com>
2022-12-26 08:52:19 +0100

    Initial commit

---
+++ app/src/main/java/com/nassim/calculer_detail_monetaire/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nassim.calculer_detail_monetaire;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
